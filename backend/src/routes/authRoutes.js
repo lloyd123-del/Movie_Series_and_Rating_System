6f39c83a1806716e23a83ab1124ed437e83ff691
AngeloHasNoEnemies <angeloburlatgg@gmail.com>
2025-11-11 04:22:32 +0800

    Models and Bcrypt

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,49 @@
 import express from "express";
+import User from "../models/User";
 
 const router = express.Router();
 
 router.get("/register", async(req, res) => {
-    res.send("register");
+    try {
+        const {username, email, password} = req.body;
+
+        if(!username || !email || !password){
+            return res.status(400).json({message: "All fields are required"})
+        }
+
+        if(password.length < 6){
+            return res.status(400).json({message:"Password should be at least 6 characters long"})
+        }
+
+        if(username.length < 6){
+            return res.status(400).json({message:"Username should be at least 6 characters long"})
+        }
+
+
+        const existingEmail = await User.findOne({email});
+        if (existingEmail){
+            return res.status(400).json({message: "Email Already Taken"})
+        }
+        const existingUsername = await User.findOne({email});
+        if (existingUsername){
+            return res.status(400).json({message: "Username Already Taken"})
+        }
+
+        const profileImage = `https://api.dicebear.com/9.x/big-ears-neutral/svg?=${username}`;
+
+        const user = new User({
+            username,
+            email,
+            password,
+            profileImage,
+        });
+
+        await user.save();
+
+        
+    } catch (error) {
+        
+    }
 });
 
 router.get("/login", async(req, res) => {
